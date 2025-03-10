54ec3aff0df8f14934903a5fd4d5f1749adaef79
Vadim <58843818+vadim-shev@users.noreply.github.com>
2025-03-10 17:19:33 +0200

    Update footer.js

--- templates/footer.js
+++ templates/footer.js
@@ -8,6 +8,7 @@ export default {
             <div class="contact_text-holder">
                 <b style="color: var(--color-1); text-transform: uppercase;"> почта: <a class="contact_mail" href="mailto:vadim.shevchenko2022@icloud.com?subject=GRANITBLIZKIM&body=хочу заказать">mail</a> </b>
                 <b style="color: var(--color-1); text-transform: uppercase;"> телефон: <a class="contact_mail" href="tel:+380974847272">+38(097)-484-72-72</a> </b>
+                <b style="color: var(--color-1); text-transform: uppercase;"> телефон: <a class="contact_mail" href="tel:+380637391111">+38(063)-739-11-11</a> </b>
                 <b style="color: var(--color-1); text-transform: uppercase;"> адресс: <a class="contact_mail" >г. Днепр</a> </b>
             </div>
             <section class="contact_info_container" >
